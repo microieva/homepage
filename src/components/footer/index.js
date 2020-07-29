3f1c628e228a8c644ec564c83e3abc8061b28435
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-07-29 20:38:30 +0300

    saving progress. err:tooltip text

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,13 +1,59 @@
 import React from 'react'
 import footerStyles from './styles.module.scss'
 
+const copyPhoneNumber = () => {
+  const tooltipText = document.querySelector('span')
+  const phone = document.querySelector('#phoneNumber')
+  let phoneNumber = phone.textContent.slice(0, -4)
+  let textArea = document.createElement('input')
+  textArea.value = phoneNumber
+  document.body.appendChild(textArea)
+  textArea.select()
+  document.execCommand('copy')
+  textArea.remove()
+
+  tooltipText.textContent = 'Copied!'
+  setInterval(() => {
+    tooltipText.textContent = 'Copy'
+  }, 1000)
+  console.log('copied phone number')
+}
+
+const copyEmail = () => {
+  const tooltipText = document.querySelector('span')
+  const email = document.querySelector('#email')
+  let emailAddress = email.textContent.slice(0, -4)
+  let textArea = document.createElement('input')
+  textArea.value = emailAddress
+  document.body.appendChild(textArea)
+  textArea.select()
+  document.execCommand('copy')
+  textArea.remove()
+
+  tooltipText.textContent = 'Copied!'
+  setInterval(() => {
+    tooltipText.textContent = 'Copy'
+  }, 1000)
+  console.log('copied email address')
+}
+
 const Footer = () => {
   return (
     <footer className={footerStyles.footer}>
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
         <p>contact me:</p>
-        <p>+35840 5899 590</p>
-        <p>ieva.vyliaudaite@me.com</p>
+        <div
+          id="phoneNumber"
+          className={footerStyles.tooltip}
+          onClick={copyPhoneNumber}
+        >
+          +358405899590
+          <span className={footerStyles.tooltiptext}>Copy</span>
+        </div>
+        <div id="email" className={footerStyles.tooltip} onClick={copyEmail}>
+          ieva.vyliaudaite@me.com
+          <span className={footerStyles.tooltiptext}>Copy</span>
+        </div>
       </div>
       <div style={{ textAlign: 'right' }}>
         <p>
