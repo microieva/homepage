8fe311755774d4dfaaf09ea26adcca6cb1c298ed
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-10-12 13:31:05 +0300

    resp fixing

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
+      options: {"plugins":[],"google":{"families":["Poiret One","Unica One","Righteous","Julius Sans One"]}},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-canonical-urls/gatsby-browser.js'),
+      options: {"plugins":[],"siteUrl":"https://ievavyliaudaite.herokuapp.com"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
